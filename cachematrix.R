628b2ae50775003e659dd1f3037f4c1757d4c180
Andreas Teneketzoglou <antekai00@gmail.com>
2014-07-25 15:20:12 +0300

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The code creates a list of functions.
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the internal variable to null
+  inverseMatrix <- NULL 
+  ## creating the set function to assign the incoming matrix to the internal variable
+  set <- function(y) { 
+    x <<- y
+    ## clearing the internal variable
+    inverseMatrix <<- NULL #
+  }
+  ## reading the value the the incoming matrix to the variable get
+  get <- function() x 
+  ## assigning the internal variable to the cacheSolve variable
+  setsolve <- function(solve) {
+    inverseMatrix <<- solve 
+  }
+  ## reassigning the internal variables, the keep all the variables
+  getsolve <- function() inverseMatrix 
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## The following set of instructions create the inverse matrix using the solve 
+## function and also stores the value for quick recall of the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## receiving the incoming matrix
+  inverseMatrix <- x$getsolve() 
+  ## if the internal variable is not null, the following message will be passed
+  ## along with the cached data, saving cpu time
+  ## checking the value of the internal variable
+  if(!is.null(inverseMatrix)) { 
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  
+  ## These following set of instructions are executed only when a new variable is passed
+  
+  ## reading the matrix into a local variable data
+  data <- x$get() 
+  ## performing the time/cpu sensitive operation solve to invert the matrix
+  inverseMatrix <- solve(data, ...) 
+  ## assigning the calculated value to the solved variable
+  x$setsolve(inverseMatrix) 
+  ## returning the internal variable with the solved variable to the parent function
+  inverseMatrix 
 }
